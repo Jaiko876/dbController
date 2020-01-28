847710aa4905120a0f2ea8baf135c40531fc907d
Jaiko876 <Jaiko876@gmail.com>
2020-01-28 18:52:40 +0300

    Some fixes

--- src/main/java/ru/quick/approval/system/dbcontroller/controller/ProcessController.java
+++ src/main/java/ru/quick/approval/system/dbcontroller/controller/ProcessController.java
@@ -20,6 +20,8 @@ import java.util.List;
 
 @RestController
 public class ProcessController implements ProcessApi {
+    private static final HttpStatus OK = HttpStatus.OK;
+    private static final HttpStatus CONFLICT = HttpStatus.CONFLICT;
     private final ProcessService processService;
 
     @Autowired
@@ -30,60 +32,60 @@ public class ProcessController implements ProcessApi {
     @Override
     public ResponseEntity<Void> createProcessByProcessType(Integer id, @Valid Process process) {
         if (processService.createNewProcessByProcessType(id, process))
-            return new ResponseEntity<>(HttpStatus.ACCEPTED);
-        return new ResponseEntity<>(HttpStatus.CONFLICT);
+            return new ResponseEntity<>(OK);
+        return new ResponseEntity<>(CONFLICT);
     }
 
     @Override
     public ResponseEntity<Void> createProcessTaskByRoleId(Integer id, Integer roleId, @Valid Task task) {
         if (processService.createNewTaskByRoleId(id, roleId, task))
-            return new ResponseEntity<>(HttpStatus.ACCEPTED);
-        return new ResponseEntity<>(HttpStatus.CONFLICT);
+            return new ResponseEntity<>(OK);
+        return new ResponseEntity<>(CONFLICT);
     }
 
     @Override
     public ResponseEntity<Void> createProcessTaskByUserId(Integer id, Integer userId, @Valid Task task) {
         if (processService.createNewTaskByUserId(id, userId, task))
-            return new ResponseEntity<>(HttpStatus.ACCEPTED);
-        return new ResponseEntity<>(HttpStatus.CONFLICT);
+            return new ResponseEntity<>(OK);
+        return new ResponseEntity<>(CONFLICT);
     }
 
     @Override
     public ResponseEntity<List<Process>> getAllActiveProcess() {
-        return new ResponseEntity<>(processService.getProcessStatusActive(),HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(processService.getProcessStatusActive(), OK);
     }
 
     @Override
     public ResponseEntity<List<Process>> getAllCompleteProcess() {
-        return new ResponseEntity<>(processService.getProcessStatusComplete(),HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(processService.getProcessStatusComplete(), OK);
 
     }
 
     @Override
     public ResponseEntity<List<Process>> getAllProcess() {
-        return new ResponseEntity<>(processService.getAllProcesses(),HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(processService.getAllProcesses(), OK);
 
     }
 
     @Override
     public ResponseEntity<List<Task>> getAllTasksByThisId(Integer id) {
-        return new ResponseEntity<>(processService.getAllTaskByProcessId(id),HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(processService.getAllTaskByProcessId(id), OK);
     }
 
     @Override
     public ResponseEntity<Process> getProcessById(Integer id) {
-        return new ResponseEntity<>(processService.getProcessById(id),HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(processService.getProcessById(id), OK);
     }
 
     @Override
     public ResponseEntity<StatusType> getProcessStatus(Integer id) {
-        return new ResponseEntity<>(processService.getProcessStatusById(id),HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(processService.getProcessStatusById(id), OK);
     }
 
     @Override
     public ResponseEntity<Void> updateProcessById(Integer id, @Valid Process process) {
         if (processService.updateProcessById(id, process))
-            return new ResponseEntity<>(HttpStatus.ACCEPTED);
-        return new ResponseEntity<>(HttpStatus.CONFLICT);
+            return new ResponseEntity<>(OK);
+        return new ResponseEntity<>(CONFLICT);
     }
 }
